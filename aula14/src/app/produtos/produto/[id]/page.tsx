14a434abb3542f219edab0d5c27a353c40bd6651
siqgabriel <gabriel.agos@live.com>
2024-09-27 23:01:42 -0300

    GetAll e GetById

--- aula14/src/app/produtos/produto/[id]/page.tsx
+++ aula14/src/app/produtos/produto/[id]/page.tsx
@@ -1,12 +1,37 @@
+"use client"
+import { useEffect, useState } from "react"
+import { TipoProduto } from "@/types"
 
 export default function Produto({params}:{params:{id:number}}) {
 
+  const [produto, setProduto] = useState<TipoProduto>({
+    id: 0,
+    nome: "",
+    preco: 0,
+    qtd: 0
+  });
+
+  useEffect(() => {
+    const consumoApi = async () => {
+      const response = await fetch(`http://localhost:3000/api/base-produtos/${params.id}`);
+      const dados = await response.json();
+      setProduto(dados);
+    }
+
+    consumoApi();
+  }, [params.id])
+  
     
 
   return (
     <div>
         <h2>Produto</h2>
-        <p>ID : {params.id}</p>
+        <div>
+          <p>ID : {produto.id}</p>
+          <p>Nome : {produto.nome}</p>
+          <p>Preço : {produto.preco}</p>
+          <p>Quantidade : {produto.qtd}</p>
+        </div>
     </div>
   )
 }

7f666cfe2d7b8a45416b23c7462b098d280e2cf7
davignz <werdavid1@gmail.com>
2024-09-27 23:03:42 -0300

    add endpoint com id

--- aula14/src/app/produtos/produto/[id]/page.tsx
+++ aula14/src/app/produtos/produto/[id]/page.tsx
@@ -1,12 +1,34 @@
+"use client";
+import { TipoProduto } from "@/types";
+import { useEffect, useState } from "react";
 
-export default function Produto({params}:{params:{id:number}}) {
+export default function Produto({ params }: { params: { id: number } }) {
+  const [produtos, setProdutos] = useState<TipoProduto>({
+    id: 0,
+    nome: "",
+    preco: 0,
+    qtd: 0,
+  });
 
-    
+  useEffect(() => {
+    const consumoApi = async () => {
+      const response = await fetch(
+        `http://localhost:3000/api/base-produtos/${params.id}`
+      );
+      const dados = await response.json();
+      setProdutos(dados);
+    };
+
+    consumoApi();
+  }, []);
 
   return (
     <div>
-        <h2>Produto</h2>
-        <p>ID : {params.id}</p>
+      <h2>Produtos</h2>
+      <h3>{produtos?.id}</h3>
+      <p>{produtos?.nome}</p>
+      <p>{produtos?.preco}</p>
+      <p>{produtos?.qtd}</p>
     </div>
-  )
+  );
 }

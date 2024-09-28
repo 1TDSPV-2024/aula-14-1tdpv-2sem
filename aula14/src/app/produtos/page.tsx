45009ce3184f7e4102da39d31516527cd2f19de4
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-27 23:01:22 -0300

    Consumo de api GetByID

--- aula14/src/app/produtos/page.tsx
+++ aula14/src/app/produtos/page.tsx
@@ -1,48 +1,56 @@
-import Link from "next/link"
+"use client";
+import { TipoProduto } from "@/types";
+import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function Produtos() {
-    const produtos = [
-        {id: 1, nome: "Mouse", preco: 100.0, qtd: 3 },
-        {id: 2, nome: "Monitor", preco: 760.5, qtd: 1 },
-        {id: 3, nome: "Headset", preco: 250.0, qtd: 2 },
-        {id: 4, nome: "Teclado", preco: 1.0, qtd: 1 },
-        {id: 5, nome: "Cabo LAN", preco: 5.0, qtd: 5 }
-    ]
+  const [produtos, setProdutos] = useState<TipoProduto[]>([]);
 
-    return (
-        <div>
-            <h2>Produtos</h2>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>Nome</th>
-                        <th>Preço</th>
-                        <th>Quantidade</th>
-                        <th>Editar</th>
-                    </tr>
-                </thead>
+  useEffect(() => {
+    const consumoApi = async () => {
+      const response = await fetch("http://localhost:3000/api/base-produtos");
+      const dados = await response.json();
+      setProdutos(dados);
+    };
 
-                <tbody>
-                    {produtos.map((p) => (
-                        <tr key={p.id}>
-                            <td>{p.id}</td>
-                            <td>{p.nome}</td>
-                            <td>{p.preco}</td>
-                            <td>{p.qtd}</td>
-                            <td><Link href={`/produtos/produto/${p.id}`}>Editar</Link></td>
-                        </tr>
-                    ))}
-                </tbody>
+    consumoApi();
+  }, []);
 
-                <tfoot>
-                    <tr>
-                        <th>
+  return (
+    <div>
+      <h2>Produtos</h2>
 
-                        </th>
-                    </tr>
-                </tfoot>
-            </table>
-        </div>
-    )
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Nome</th>
+            <th>Preço</th>
+            <th>Quantidade</th>
+            <th>Editar</th>
+          </tr>
+        </thead>
+
+        <tbody>
+          {produtos.map((p) => (
+            <tr key={p.id}>
+              <td>{p.id}</td>
+              <td>{p.nome}</td>
+              <td>{p.preco}</td>
+              <td>{p.qtd}</td>
+              <td>
+                <Link href={`/produtos/produto/${p.id}`}>Editar</Link>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+
+        <tfoot>
+          <tr>
+            <td colSpan={5}>Total de registros : {produtos.length}</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
 }
